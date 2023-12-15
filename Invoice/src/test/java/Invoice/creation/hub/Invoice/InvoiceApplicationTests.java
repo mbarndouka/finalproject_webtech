607ce7580a0ae61000348b926fbf54c2e2aa7f4a
mbarndouka <mmbarndouka@gmail.com>
2023-12-15 11:08:56 +0200

    backend setup for invoice

---
+++ Invoice/src/test/java/Invoice/creation/hub/Invoice/InvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Invoice.creation.hub.Invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
